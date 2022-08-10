2d4465ec66b0eff536171747424caed4476a9555
Ritik <ritikraj0018@gmail.com>
2022-08-11 01:27:14 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import CryptoContext from './CryptoContext';
+import 'react-alice-carousel/lib/alice-carousel.css';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
+ReactDOM.render(
+
+    <CryptoContext>
     <App />
-  </React.StrictMode>
+    </CryptoContext>,
+  document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
